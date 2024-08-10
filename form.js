742e3a43aca642be7f70401edb9d75276192c551
Ojingiri Samuel <89736223+ojiiis@users.noreply.github.com>
2024-08-10 15:58:35 +0100

    Update form.js

--- form.js
+++ form.js
@@ -7,6 +7,19 @@ const form = ojp.ojparty;
 const serve = http.createServer((req,res)=>{
     form.forms(req,(body)=>{
      console.log(body)
+        /*
+       output
+       {
+       body:{key:value,key:value},
+       query:{key:value,key:value},
+       url:"url string without query",
+       files:[
+          0:{filedata},   
+          1:{filedata}
+       ]
+       }
+
+        */
     });
 
 });
